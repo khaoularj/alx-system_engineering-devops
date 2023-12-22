585d6875dc43179e163eef45b2ace7ecb721b89b
khaoularj <khaoula.raja1@gmail.com>
2023-12-22 07:30:13 -0800

    installing a package using puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 #!/usr/bin/pup
 #install a package using puppet
 
+package {'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3'
+}
+
 package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
