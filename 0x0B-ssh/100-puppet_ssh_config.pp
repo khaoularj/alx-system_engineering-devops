95cde990991b4ce8e617e8e88bb317448d479320
khaoularj <khaoula.raja1@gmail.com>
2023-12-28 04:46:19 -0800

    using puppet to change configuration file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,8 +3,9 @@
 #SSH client configuration must be configured to use the private key ~/.ssh/school
 #SSH client configuration must be configured to refuse to authenticate using a password
 
-file {'etc/ssh/ssh_config':
+file {'/etc/ssh/ssh_config':
   ensure  => present,
+  path    => '/etc/ssh/ssh_config',
   content =>"
     Host *
     PasswordAuthentication no
