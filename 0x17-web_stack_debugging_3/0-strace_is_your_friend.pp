763a3c2cec67edacf8cd02feba58dd2d130d227f
khaoularj <khaoula.raja1@gmail.com>
2024-02-14 12:08:00 -0800

    using strace to find out where the bug

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+#this script is for fixing the phpp extens to just php
+
+exec{'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
