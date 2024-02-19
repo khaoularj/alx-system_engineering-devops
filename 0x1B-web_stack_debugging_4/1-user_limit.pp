053483ff9336fe3051d4d3849f7be58088f3cd24
khaoularj <khaoula.raja1@gmail.com>
2024-02-19 07:25:26 -0800

    enable user Holberton

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,12 @@
+#this puppet enable to use the user holberton to login and open files without any error
+
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => "sed -i '/^holberton hard/s/5/50000/' /etc/security/limits.conf",
+  path    => '/usr/local/bin/:/bin/'
+}
+
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/^holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
